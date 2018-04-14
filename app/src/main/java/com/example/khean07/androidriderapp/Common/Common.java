c326a6e686b0a9c1d57bbef95859ebecb4f626a9
Marvin Fontanilla <marvinfontanilla14@gmail.com>
2018-04-14 22:23:48 +0800

    first commit

---
+++ app/src/main/java/com/example/khean07/androidriderapp/Common/Common.java
@@ -0,0 +1,14 @@
+package com.example.khean07.androidriderapp.Common;
+
+/**
+ * Created by Khean07 on 4/9/2018.
+ */
+
+public class Common {
+
+    public static final String driver_tbl = "Drivers";
+    public static final String user_driver_tbl = "DriversInformation";
+    public static final String user_rider_tbl = "RidersInformation";
+    public static final String pickup_request_tbl = "PickupRequest";
+
+}
