0b5de8247ab7727ed9297653e1ab4e22e957c26d
Marvin Fontanilla <marvinfontanilla14@gmail.com>
2018-04-23 22:28:53 +0800

    updates

--- app/src/main/java/com/example/khean07/androidriderapp/Home.java
+++ app/src/main/java/com/example/khean07/androidriderapp/Home.java
@@ -214,8 +214,8 @@ public class Home extends AppCompatActivity
 
                     // make raw payload - convert latling to json
                     String json_lat_lng = new Gson().toJson(new LatLng(mLastLocation.getLatitude(),mLastLocation.getLongitude()));
-
-                    Notification notification = new Notification("EDMTDEV",json_lat_lng); // send it to Driver app and we will deserialize it again
+                    String riderToken = FirebaseInstanceId.getInstance().getToken();
+                    Notification notification = new Notification(riderToken,json_lat_lng); // send it to Driver app and we will deserialize it again
                     Sender content = new Sender(notification, token.getToken()); // Send this notification to token
 
                     mService.sendMessage(content)
--- app/src/main/java/com/example/khean07/androidriderapp/Service/MyFirebaseMessaging.java
+++ app/src/main/java/com/example/khean07/androidriderapp/Service/MyFirebaseMessaging.java
@@ -1,5 +1,9 @@
 package com.example.khean07.androidriderapp.Service;
 
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -10,16 +14,16 @@ import com.google.firebase.messaging.RemoteMessage;
 public class MyFirebaseMessaging extends FirebaseMessagingService {
 
     @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        // Because i will send the firebase message with contain lat and lng from rider app
-        //So i need covert message to LatLng
-//        LatLng customer_location = new Gson().fromJson(remoteMessage.getNotification().getBody(),LatLng.class);
-//
-//        Intent intent = new Intent(getBaseContext(), CustommerCall.class);
-//        intent.putExtra("lat",customer_location.latitude);
-//        intent.putExtra("lng",customer_location.longitude);
-//
-//        startActivity(intent);
+    public void onMessageReceived(final RemoteMessage remoteMessage) {
+        // Because this is outsusde of main thread, so if you want to run toast, you need create hanler to do that
+        // choose handler from android.os
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MyFirebaseMessaging.this,""+remoteMessage.getNotification().getBody(),Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
